3c2dcf41d88ecca182d598748ad07afbe234dbf7
Samvel Arakelyan <samvel.arakelyan00@gmail.com>
2025-01-21 18:52:32 +0400

    chagne front folder name to add it backend

--- UserSide/app/main.py
+++ UserSide/app/main.py
@@ -7,7 +7,7 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="../../cafeMenu-example/static"), name="static")
+app.mount("/static", StaticFiles(directory="../../cafeMenu-example2/static"), name="static")
 
 
 origins = [
@@ -30,24 +30,24 @@ def main():
 
 @app.get("/index")
 def index():
-    return FileResponse("../../cafeMenu-example/index.html")
+    return FileResponse("../../cafeMenu-example2/index.html")
 
 
 @app.get("/appetizers")
 def get_appetizers_html_page():
-    return FileResponse("../../cafeMenu-example/appetizers.html")
+    return FileResponse("../../cafeMenu-example2/appetizers.html")
 
 
 @app.get("/desserts")
 def get_desserts_html_page():
-    return FileResponse("../../cafeMenu-example/desserts.html")
+    return FileResponse("../../cafeMenu-example2/desserts.html")
 
 
 @app.get("/drinks")
 def get_drinks_html_page():
-    return FileResponse("../../cafeMenu-example/drinks.html")
+    return FileResponse("../../cafeMenu-example2/drinks.html")
 
 
 @app.get("/main-dishes")
 def get_main_dishes_html_page():
-    return FileResponse("../../cafeMenu-example/main-dishes.html")
+    return FileResponse("../../cafeMenu-example2/main-dishes.html")
