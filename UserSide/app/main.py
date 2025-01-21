1b637ffc0a49892805d6d21ced2f74d26487e598
Samvel Arakelyan <samvel.arakelyan00@gmail.com>
2025-01-21 18:39:38 +0400

    add front end and config it to work with bakcned, add some APIs for that

--- UserSide/app/main.py
+++ UserSide/app/main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="../../cafeMenu-example/static"), name="static")
 
 
 origins = [
@@ -21,3 +26,28 @@ app.add_middleware(
 @app.get("/")
 def main():
     return "OK"
+
+
+@app.get("/index")
+def index():
+    return FileResponse("../../cafeMenu-example/index.html")
+
+
+@app.get("/appetizers")
+def get_appetizers_html_page():
+    return FileResponse("../../cafeMenu-example/appetizers.html")
+
+
+@app.get("/desserts")
+def get_desserts_html_page():
+    return FileResponse("../../cafeMenu-example/desserts.html")
+
+
+@app.get("/drinks")
+def get_drinks_html_page():
+    return FileResponse("../../cafeMenu-example/drinks.html")
+
+
+@app.get("/main-dishes")
+def get_main_dishes_html_page():
+    return FileResponse("../../cafeMenu-example/main-dishes.html")
---
+++ UserSide/app/services/create_QR_for_menu_open.py
@@ -0,0 +1,24 @@
+import qrcode
+
+# Data to encode in the QR code
+data = "http://192.168.10.110:8000/index"
+
+# Create a QR code object with specific settings
+qr = qrcode.QRCode(
+    version=1,  # Controls the size of the QR Code (1 is the smallest)
+    error_correction=qrcode.constants.ERROR_CORRECT_L,  # Error correction level
+    box_size=10,  # Size of each box in pixels
+    border=4,  # Thickness of the border (minimum is 4)
+)
+
+# Add data to the QR code
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image of the QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image to a file
+img.save("qrcode.png")
+
+print("QR Code saved as qrcode.png")
