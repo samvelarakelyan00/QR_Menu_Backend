b0f6cfd0eb2f1cda4c69e9ccaa7065dc4af81fd9
Samvel Arakelyan <samvel.arakelyan00@gmail.com>
2025-02-15 21:15:26 +0400

    menu get from front

--- Cafe-Menu/static/scripts/menu.js
+++ Cafe-Menu/static/scripts/menu.js
@@ -26,23 +26,212 @@ menuLinks.forEach((link) => {
   });
 });
 
+// ----------------------------------------------------------------------
+
+const Food_API_Link =
+  "http://23.20.175.90/api/api/menu-filter/by-kind/food?horekaclient_id=";
+const foodCont = document.getElementById("foodCont");
+
+async function foodCategories(link, category) {
+  try {
+    const response = await fetch(link + localStorage.getItem("horecaID"));
+    const data = await response.json();
+
+    for (let i = 0; i < data.length; i++) {
+      foodCont.innerHTML += `<div class="item">
+                              <div class="likeBtn">
+                                <img src="../static/icons/liked.svg" alt="liked" />
+                              </div>
+                              <img
+                                src="${data[i].image_src}"
+                                alt="meal"
+                                class="item-img"
+                              />
+                              <div class="info">
+                                <p class="name">${data[i].name}</p>
+                                <p class="price">AMD${data[i].price}</p>
+                              </div>
+                            </div>`;
+    }
+  } catch (error) {
+    console.log("Error fetchind api", error);
+  }
+}
+
+foodCategories(Food_API_Link);
+
+foodCont.addEventListener("click", (event) => {
+  const item = event.target.closest(".item");
+  if (!item) return;
+
+  const name = item.querySelector(".name").textContent;
+  const price = item.querySelector(".price").textContent;
+  const imgURL = item.querySelector(".item-img").src;
+
+  const mealInfo = {
+    name,
+    price,
+    imgURL,
+  };
+
+  localStorage.setItem("selectedMeal", JSON.stringify(mealInfo));
+  window.location.href = "./about-meal.html";
+});
+
 // ------------------------------------------------------------
 
-const menuItems = document.querySelectorAll(".item");
-const menuItemsNames = document.querySelectorAll(".name");
-const menuItemsPrices = document.querySelectorAll(".price");
-const menuItemsImages = document.querySelectorAll(".item-img");
+const Salad_API_Link =
+  "http://23.20.175.90/api/api/menu-filter/by-kind/salad?horekaclient_id=";
+const saladCont = document.getElementById("saladCont");
 
-menuItems.forEach((item, index) => {
-  item.addEventListener("click", () => {
-    const mealInfo = {
-      name: menuItemsNames[index].textContent,
-      price: menuItemsPrices[index].textContent,
-      imgURL: menuItemsImages[index].src,
-    };
+async function saladCategories(link) {
+  try {
+    const response = await fetch(link + localStorage.getItem("horecaID"));
+    const data = await response.json();
 
-    localStorage.setItem("selectedMeal", JSON.stringify(mealInfo));
+    for (let i = 0; i < data.length; i++) {
+      saladCont.innerHTML += `<div class="item">
+                              <div class="likeBtn">
+                                <img src="../static/icons/liked.svg" alt="liked" />
+                              </div>
+                              <img
+                                src="${data[i].image_src}"
+                                alt="meal"
+                                class="item-img"
+                              />
+                              <div class="info">
+                                <p class="name">${data[i].name}</p>
+                                <p class="price">AMD${data[i].price}</p>
+                              </div>
+                            </div>`;
+    }
+  } catch (error) {
+    console.log("Error fetchind api", error);
+  }
+}
 
-    window.location.href = "./about-meal.html";
-  });
+saladCategories(Salad_API_Link);
+
+saladCont.addEventListener("click", (event) => {
+  const item = event.target.closest(".item");
+  if (!item) return;
+
+  const name = item.querySelector(".name").textContent;
+  const price = item.querySelector(".price").textContent;
+  const imgURL = item.querySelector(".item-img").src;
+
+  const mealInfo = {
+    name,
+    price,
+    imgURL,
+  };
+
+  localStorage.setItem("selectedMeal", JSON.stringify(mealInfo));
+  window.location.href = "./about-meal.html";
 });
+
+// ------------------------------------------------------------
+
+const Drink_API_Link =
+  "http://23.20.175.90/api/api/menu-filter/by-kind/drink?horekaclient_id=";
+const drinkCont = document.getElementById("drinkCont");
+
+async function drinkCategories(link) {
+  try {
+    const response = await fetch(link + localStorage.getItem("horecaID"));
+    const data = await response.json();
+
+    for (let i = 0; i < data.length; i++) {
+      drinkCont.innerHTML += `<div class="item">
+                              <div class="likeBtn">
+                                <img src="../static/icons/liked.svg" alt="liked" />
+                              </div>
+                              <img
+                                src="${data[i].image_src}"
+                                alt="meal"
+                                class="item-img"
+                              />
+                              <div class="info">
+                                <p class="name">${data[i].name}</p>
+                                <p class="price">AMD${data[i].price}</p>
+                              </div>
+                            </div>`;
+    }
+  } catch (error) {
+    console.log("Error fetchind api", error);
+  }
+}
+
+drinkCategories(Drink_API_Link);
+
+drinkCont.addEventListener("click", (event) => {
+  const item = event.target.closest(".item");
+  if (!item) return;
+
+  const name = item.querySelector(".name").textContent;
+  const price = item.querySelector(".price").textContent;
+  const imgURL = item.querySelector(".item-img").src;
+
+  const mealInfo = {
+    name,
+    price,
+    imgURL,
+  };
+
+  localStorage.setItem("selectedMeal", JSON.stringify(mealInfo));
+  window.location.href = "./about-meal.html";
+});
+
+// ------------------------------------------------------------
+
+const Dessert_API_Link =
+  "http://23.20.175.90/api/api/menu-filter/by-kind/desert?horekaclient_id=";
+const dessertCont = document.getElementById("dessertCont");
+
+async function dessertCategories(link) {
+  try {
+    const response = await fetch(link + localStorage.getItem("horecaID"));
+    const data = await response.json();
+
+    for (let i = 0; i < data.length; i++) {
+      dessertCont.innerHTML += `<div class="item">
+                              <div class="likeBtn">
+                                <img src="../static/icons/liked.svg" alt="liked" />
+                              </div>
+                              <img
+                                src="${data[i].image_src}"
+                                alt="meal"
+                                class="item-img"
+                              />
+                              <div class="info">
+                                <p class="name">${data[i].name}</p>
+                                <p class="price">AMD${data[i].price}</p>
+                              </div>
+                            </div>`;
+    }
+  } catch (error) {
+    console.log("Error fetchind api", error);
+  }
+}
+
+dessertCategories(Dessert_API_Link);
+
+dessertCont.addEventListener("click", (event) => {
+  const item = event.target.closest(".item");
+  if (!item) return;
+
+  const name = item.querySelector(".name").textContent;
+  const price = item.querySelector(".price").textContent;
+  const imgURL = item.querySelector(".item-img").src;
+
+  const mealInfo = {
+    name,
+    price,
+    imgURL,
+  };
+
+  localStorage.setItem("selectedMeal", JSON.stringify(mealInfo));
+  window.location.href = "./about-meal.html";
+});
+
+// -------------------------------------------------------------------------
