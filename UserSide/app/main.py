4c6f8273d6bf9b5b3556c4d6c89a124c3fb06bd1
Samvel Arakelyan <samvel.arakelyan00@gmail.com>
2025-02-15 23:13:05 +0400

    menu open bug fix try

--- UserSide/app/main.py
+++ UserSide/app/main.py
@@ -1,3 +1,5 @@
+import os
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -11,7 +13,12 @@ from api import router
 
 app = FastAPI(docs_url="/api/docs/")
 
-app.mount("/static", StaticFiles(directory="../../Cafe-Menu/static"), name="static")
+# Get the absolute path to the Cafe-Menu folder
+base_dir = os.path.dirname(os.path.abspath(__file__))  # This is your backend folder
+static_dir = os.path.join(base_dir, "../..", "Cafe-Menu", "static")  # Go up one level and into Cafe-Menu
+
+# Mount static files
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 
 origins = [
