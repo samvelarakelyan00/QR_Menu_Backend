b54d68fab20cdd148280c047f811eb08953be7dd
Samvel Arakelyan <samvel.arakelyan00@gmail.com>
2025-03-09 18:47:34 +0400

    go to about meal and back to menu in front side

---
+++ CafeAdminPanel/tests/menudata.py
@@ -0,0 +1,92 @@
+datalist = [
+{
+  "kind": "food",
+  "category": "Pizza",
+  "name": "Pizza Pepperoni",
+  "quantity": "1 piece",
+  "price": 2600,
+  "description": "Pizza Pepperoni, very good Pizza Pepperoni, nice Pizza Pepperoni",
+  "image": "test_pizza_pepperoni.jpeg",
+  "language": "en"
+},
+{
+  "kind": "food",
+  "category": "Pizza",
+  "name": "Pizza Margarita",
+  "quantity": "1 piece",
+  "price": 2200,
+  "description": "Pizza Margarita, very good Pizza Margarita, nice Pizza Margarita",
+  "image": "test_pizza_margarita.jpeg",
+  "language": "en"
+},
+{
+  "kind": "drink",
+  "category": "Cocltail",
+  "name": "Long Island",
+  "quantity": "1 cup",
+  "price": 3400,
+  "description": "Long Island, very good Long Island, nice Long Island",
+  "image": "test_cocktail_longisland.jpg",
+  "language": "en"
+},
+{
+  "kind": "drink",
+  "category": "Vodka",
+  "name": "Absolut",
+  "quantity": "1 bottle",
+  "price": 13500,
+  "description": "Absolut, very good Absolut, nice Absolut",
+  "image": "test_vodka_absolut.jpg",
+  "language": "en"
+},
+{
+  "kind": "drink",
+  "category": "Beer",
+  "name": "Stella",
+  "quantity": "1 bottle",
+  "price": 1000,
+  "description": "Stella, very good Stella, nice Stella",
+  "image": "test_beer_stella.jpg",
+  "language": "en"
+},
+{
+  "kind": "drink",
+  "category": "Soda Drink",
+  "name": "Coca-cola",
+  "quantity": "1 bottle",
+  "price": 1500,
+  "description": "Coca-cola, very good Coca-cola, nice Coca-cola",
+  "image": "test_sodadrink_cocacola.webp",
+  "language": "en"
+},
+{
+  "kind": "dessert",
+  "category": "Cake",
+  "name": "Brownie",
+  "quantity": "1 peace",
+  "price": 800,
+  "description": "Brownie, very good Brownie, nice Brownie",
+  "image": "test_dessert_brownie.jpg",
+  "language": "en"
+},
+{
+  "kind": "dessert",
+  "category": "Icecream",
+  "name": "Gelato",
+  "quantity": "1 peace",
+  "price": 800,
+  "description": "Gelato, very good Gelato, nice Gelato",
+  "image": "test_icecream_chocolategelato.jpg",
+  "language": "en"
+},
+{
+  "kind": "salad",
+  "category": "Green Salad",
+  "name": "Big Italian Salad",
+  "quantity": "1 pate",
+  "price": 1200,
+  "description": "Italian Salad, very good Italian Salad, nice Italian Salad",
+  "image": "test_salad_bigitaliansalad.jpg",
+  "language": "en"
+}
+]
---
+++ CafeAdminPanel/tests/producttomenu.py
@@ -0,0 +1,50 @@
+import os
+import time
+
+import requests
+
+from menudata import datalist
+
+
+def add_menu_new(url: str, form_data: dict, file_path: str = "", token: str=""):
+    # Open the image file
+    with open(file_path, "rb") as image_file:
+        files = {"image": image_file}
+
+        # Set headers with Bearer token
+        headers = {
+            "Authorization": f"Bearer {token}"
+        }
+
+        # Send POST request
+        response = requests.post(url, data=form_data, files=files, headers=headers)
+
+        # Handle response
+        if response.status_code == 200:
+            print("Menu item added:", response.json())
+        else:
+            print(f"Error: {response.status_code} - {response.text}")
+
+
+# localhost url
+# url = "http://127.0.0.1:8080/api/menuCRUD/add-menu-new"
+
+# localhost token
+# auth_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDE1OTg4NDYsImNhZmVfYWRtaW4iOnsiaWQiOjEsIm5hbWUiOiJ0bWJsIEFybWVuIiwiZW1haWwiOiJ0bWJsQGdtYWlsLmNvbSIsImhvcmVrYWNsaWVudF9pZCI6MX19.BXdevTgePjNQcU-9CRrHvNVoQyHb43m-MHft_6yFBfY"
+
+# aws test ec2 url
+url = "http://23.20.175.90/api/cafeadmin/api/menuCRUD/add-menu-new"
+# aws test ec2 token
+auth_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDE2MDMyMjYsImNhZmVfYWRtaW4iOnsiaWQiOjEsIm5hbWUiOiJtZWxvZHlfYWRtaW4iLCJlbWFpbCI6Im1lbG9keUBnbWFpbC5jb20iLCJob3Jla2FjbGllbnRfaWQiOjF9fQ.QDspEsdArSVJEv_L19DdtB7FLa0Mi9T2om7UvT143Ik"
+image_directory = "../../../../images/"
+
+for form_data in datalist:
+    # Construct full image file path
+    image_path = os.path.join(image_directory, form_data["image"])
+
+    if not os.path.exists(image_path):
+        print(f"Warning: Image file not found for {form_data['name']} at {image_path}")
+        continue  # Skip if image does not exist
+
+    add_menu_new(url, form_data, image_path, auth_token)
+    time.sleep(1)  # Avoid overwhelming the server
