d92b88e84499494a006a81f10969d52cea78b78a
Samvel Arakelyan <samvel.arakelyan00@gmail.com>
2025-04-02 16:02:03 +0400

    alembic reinit init db

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,6 @@ from sqlalchemy import pool
 from alembic import context
 
 from OurAdminPanel.app.models.models import Base
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,8 +19,8 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-# target_metadata = None
 target_metadata = Base.metadata
+
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
